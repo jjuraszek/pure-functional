19f1d5e1eb77a32cb29084fd92fb02e824247839
Jacek Juraszek <jacek.juraszek@allegro.pl>
2014-05-05 08:34:15 +0200

    init uncomplete euclidean algorith for max common denom

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject pure-functional "0.1.0-SNAPSHOT"
+  :description "Classic algorithms in clojure"
+  :url "https://github.com/jjuraszek/pure-functional"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
---
+++ src/pure_functional/simple.clj
@@ -0,0 +1,7 @@
+(ns pure-functional.simple
+  (:gen-class))
+
+(defn common-denominator
+  "Count maximum number which is common denominator of two of them"
+  [a b]
+  b)
