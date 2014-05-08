cfdfa3b36a5a08820ed0d11c10e0c832eaffabc2
Jacek Juraszek <jacek.juraszek@gmail.com>
2014-05-08 22:38:07 +0200

    matrix multiplication

--- src/pure_functional/math.clj
+++ src/pure_functional/math.clj
@@ -10,3 +10,20 @@
         higher
         #(euclids-max-common-denom lower (mod higher lower)))))
   (trampoline euclids-max-common-denom (max a b) (min a b)))
+
+(defn multiply-matrix
+  "multiply 2 compatible matrixs"
+  [matrix1 matrix2]
+  (def value-of-field 
+    (fn [row col]
+      (reduce +
+        (map-indexed 
+          (fn [columnIdx leftValue]
+            (* leftValue (nth (nth matrix2 columnIdx) col)))
+          (nth matrix1 row)))))
+  (map
+    (fn [rowIdx] 
+      (map 
+        #(value-of-field rowIdx %)
+        (range (count (first matrix2))))) 
+    (range (count matrix1))))
--- test/pure_functional/math_test.clj
+++ test/pure_functional/math_test.clj
@@ -11,3 +11,13 @@
     (is (= 2 (max-common-denominator 4 10))))
   (testing "for work for large number"
     (is (= 18 (max-common-denominator 461952 116298)))))
+
+(deftest multiply-matrix-test
+  (testing "should return multiplication of two numbers for 1x1 * 1x1"
+    (is (= [[6]] (multiply-matrix [[2]] [[3]]))))
+  (testing "should multiply two vectors 1xn and nx1 and accumulate it to one number"
+    (is (= [[20]] (multiply-matrix [[2 3 1]] [[5][2][4]]))))
+  (testing "should multiply matrix 3x2 by 2x1"
+    (is (= [[23][24][77]] (multiply-matrix [[2 3][1 4][8 9]] [[4][5]]))))
+  (testing "should multiply matrix 2x2 by 2x3"
+    (is (= [[90 120 150][190 260 330]] (multiply-matrix [[10 20][30 40]] [[1 2 3][4 5 6]])))))
