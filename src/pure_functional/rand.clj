5ad32a65f18c5eccd24343a333986afa1238c72c
Jacek Juraszek <jacek.juraszek@allegro.pl>
2014-05-14 14:25:23 +0200

    chi-sqrt for random generator should be around upper bound for random stream

--- src/pure_functional/rand.clj
+++ src/pure_functional/rand.clj
@@ -1,29 +1,29 @@
 (ns pure-functional.rand)
 
-(declare *mod32)
+(declare *mod48)
 
 (defn linear-congruential
   "return function which is generating random number vector on provided seed"
-  ([] (linear-congruential (.intValue (System/currentTimeMillis))))
-  ([prev] (let [seed (Math/abs (int prev))
-                m (.intValue (Math/pow 2 32))
-                multiplier 25214903917
-                increment 11
-                new-random-num (mod 
-                                  (+ 
-                                    (*mod32 seed multiplier) 
-                                    increment) 
-                                  m)]
-    (cons new-random-num (lazy-seq (linear-congruential new-random-num))))))
+  ([] (linear-congruential (. Integer MAX_VALUE)))
+  ([upperBound] (linear-congruential (.longValue (System/currentTimeMillis)) upperBound))
+  ([prev upperBound] (let [seed (Math/abs (long prev))
+                           m (.longValue (Math/pow 2 48))
+                           uslessBits (Math/pow 2 16)
+                           multiplier 25214903917
+                           increment 11
+                           new-random-num (mod 
+                                            (+ (*mod48 seed multiplier) increment) 
+                                            m)]
+    (cons (mod (bit-shift-right new-random-num 16) upperBound) (lazy-seq (linear-congruential new-random-num upperBound))))))
 
-(defn *mod32
+(defn *mod48
   [a b]
-  (let [p32 (.intValue (Math/pow 2 32))
-        p16 (.intValue (Math/pow 2 16))
-        a1 (quot a p16)
-        a0 (mod a p16)
-        b1 (quot b p16)
-        b0 (mod b p16)]
+  (let [p48 (.longValue (Math/pow 2 48))
+        p24 (.longValue (Math/pow 2 24))
+        a1 (quot a p24)
+        a0 (mod a p24)
+        b1 (quot b p24)
+        b0 (mod b p24)]
     (mod 
       (+ 
         (* 
@@ -31,7 +31,7 @@
             (+
               (* a1 b0) 
               (* a0 b1))
-            p16)
-          p16)
+            p24)
+          p24)
         (* a0 b0)) 
-      p32)))
+      p48)))
--- test/pure_functional/rand_test.clj
+++ test/pure_functional/rand_test.clj
@@ -3,6 +3,7 @@
             [pure-functional.rand :refer :all]))
 
 (declare isMonotonic)
+(declare chi-square)
 
 (deftest linear-congruential-test
   (testing "should return one rand number not equal to seed"
@@ -11,13 +12,30 @@
     (is (= 10 (let [result (take 10 (linear-congruential))]
                 (count (set result))))))
   (testing "should not have numbers in monotonic order"
-    (is (not (isMonotonic (take 10 (linear-congruential)))))))
+    (is (not (isMonotonic (take 10 (linear-congruential))))))
+  (testing "should chi-square be lower then upperBound+2*upperBound^0.5"
+    (is 
+      (>
+        (+ 100 (* 2 (Math/pow 100 0.5)))
+        (let [upperBound 100
+              N 1000
+              randNumbers (take N (linear-congruential upperBound))]
+          (Math/abs (chi-square randNumbers N upperBound))
+        )))))
 
-(deftest *mod32-test
-  (testing "should a*b if a*b =< 2^32"
-    (is (= (* 4 5) (*mod32 4 5))))
-  (testing "should a*b mod 2^32 if a*b > 2^32"
-    (is (= 65536 (*mod32 (.intValue (Math/pow 2 16)) (+ 1 (.intValue (Math/pow 2 16))))))))
+(deftest *mod48-test
+  (testing "should a*b if a*b =< 2^48"
+    (is (= (* 4 5) (*mod48 4 5))))
+  (testing "should a*b mod 2^48 if a*b > 2^48"
+    (is (= 16777216 (*mod48 (.longValue (Math/pow 2 24)) (+ 1 (.longValue (Math/pow 2 24))))))))
+
+(defn chi-square
+  [randNumbers N upperBound]
+  (let [freqBucket (reduce #(assoc %1 %2 (inc (%1 %2)))  (vec (repeat upperBound 0)) randNumbers)
+        desireFreq (/ N upperBound)
+        squareError (reduce #(+ %1 (Math/pow (- desireFreq %2) 2)) 0 freqBucket)
+        chiSquareVal (/ squareError desireFreq)]
+          chiSquareVal))
 
 (defn isMonotonic
   [v]
